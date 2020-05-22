13259f2dc89cf7714aa93da1c82f6a6ff1f84eec
berkaysimsek <berkay.simsek@topia.com>
2020-05-22 19:21:39 +0300

    style change

--- src/main/java/com/example/otDemoApplication/Application.java
+++ src/main/java/com/example/otDemoApplication/Application.java
@@ -21,7 +21,7 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-    
+
     @Bean("client")
     ForecastClient weatherForecastBean() {
         JAXBContextFactory jaxbFactory = new JAXBContextFactory.Builder()
